33826b36a5e31a5a6c741b898911353b21be9e52
Dana Almoaiqel <113850532+danaifm@users.noreply.github.com>
2023-09-06 13:30:16 +0300

    splash screen fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:preggo/SignUp.dart';
 import 'package:preggo/SplashScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:preggo/SignUp.dart';
+import 'package:string_validator/string_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -10,8 +18,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // the root widget
-      home:
-          SplashScreen(), // each class representes a page or a screen, if you want to display the login class(page) you just call it form here
+      home: SplashScreen(),
+      // each class representes a page or a screen, if you want to display the login class(page) you just call it form here
     );
   }
 }
