38c9804930522070c3f98f5ae26aeebfe89d67a2
Bdoor Nasser <bdoorna7@gmail.com>
2023-10-01 13:36:59 +0300

    setting up things

---
+++ lib/viewAppointment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class viewAppointment extends StatefulWidget {
+  //const SignUp({Key? key}) : super(key: key);
+  late final String userId;
+  @override
+  State<StatefulWidget> createState() {
+    return _viewAppointment();
+  }
+}
+
+class _viewAppointment extends State<viewAppointment> {
+  @override
+  Widget build(BuildContext context) {}
+}
