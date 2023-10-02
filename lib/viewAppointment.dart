d4b131d10735b9b1c8da0c0d2063bf1fa40e6b5d
Bdoor Nasser <bdoorna7@gmail.com>
2023-10-02 23:42:20 +0300

    sign in added but there is a problem

--- lib/viewAppointment.dart
+++ lib/viewAppointment.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:googleapis/calendar/v3.dart' as GoogleAPI;
+// ignore: depend_on_referenced_packages
 import 'package:http/io_client.dart' show IOClient, IOStreamedResponse;
+// ignore: depend_on_referenced_packages
 import 'package:http/http.dart' show BaseRequest, Response;
 
 class viewAppointment extends StatefulWidget {
@@ -16,14 +18,30 @@ class viewAppointment extends StatefulWidget {
 }
 
 class _viewAppointment extends State<viewAppointment> {
-  @override
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: Text("Bdoor"),
+  //   );
+  // }
+
+  //  Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: SfCalendar(
+  //       view: CalendarView.month,
+  //       initialDisplayDate: DateTime(2023, 09, 01),
+  //     ),
+  //   );
+  // }
+
   final GoogleSignIn _googleSignIn = GoogleSignIn(
-    clientId: 'your-client_id.apps.googleusercontent.com',
+    //clientId: 'your-client_id.apps.googleusercontent.com',
     scopes: <String>[GoogleAPI.CalendarApi.calendarScope],
   );
 
   GoogleSignInAccount? _currentUser;
 
+  @override
   void initState() {
     super.initState();
     _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
@@ -37,12 +55,133 @@ class _viewAppointment extends State<viewAppointment> {
     _googleSignIn.signInSilently();
   }
 
+  Future<List<GoogleAPI.Event>> getGoogleEventsData() async {
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+    final GoogleAPIClient httpClient =
+        GoogleAPIClient(await googleUser!.authHeaders);
+
+    final GoogleAPI.CalendarApi calendarApi = GoogleAPI.CalendarApi(httpClient);
+    final GoogleAPI.Events calEvents = await calendarApi.events.list(
+      "primary",
+    );
+
+    final List<GoogleAPI.Event> appointments = <GoogleAPI.Event>[];
+    if (calEvents.items != null) {
+      for (int i = 0; i < calEvents.items!.length; i++) {
+        final GoogleAPI.Event event = calEvents.items![i];
+        if (event.start == null) {
+          continue;
+        }
+        appointments.add(event);
+      }
+    }
+
+    return appointments;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SfCalendar(
-        view: CalendarView.month,
-        initialDisplayDate: DateTime(2023, 09, 01),
+      appBar: AppBar(
+        title: const Text('Event Calendar'),
+      ),
+      body: FutureBuilder(
+        future: getGoogleEventsData(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          return Stack(
+            children: [
+              SfCalendar(
+                view: CalendarView.month,
+                initialDisplayDate: DateTime(
+                  2023,
+                  8,
+                  1,
+                ),
+                dataSource: GoogleDataSource(events: snapshot.data),
+                monthViewSettings: const MonthViewSettings(
+                    appointmentDisplayMode:
+                        MonthAppointmentDisplayMode.appointment),
+              ),
+              snapshot.data != null
+                  ? Container()
+                  : const Center(
+                      child: CircularProgressIndicator(),
+                    )
+            ],
+          );
+        },
       ),
     );
   }
+
+  @override
+  void dispose() {
+    if (_googleSignIn.currentUser != null) {
+      _googleSignIn.disconnect();
+      _googleSignIn.signOut();
+    }
+
+    super.dispose();
+  }
+}
+
+class GoogleDataSource extends CalendarDataSource {
+  GoogleDataSource({required List<GoogleAPI.Event>? events}) {
+    appointments = events;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.start?.date ?? event.start!.dateTime!.toLocal();
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].start.date != null;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.endTimeUnspecified != null && event.endTimeUnspecified!
+        ? (event.start?.date ?? event.start!.dateTime!.toLocal())
+        : (event.end?.date != null
+            ? event.end!.date!.add(const Duration(days: -1))
+            : event.end!.dateTime!.toLocal());
+  }
+
+  @override
+  String getLocation(int index) {
+    return appointments![index].location ?? '';
+  }
+
+  @override
+  String getNotes(int index) {
+    return appointments![index].description ?? '';
+  }
+
+  @override
+  String getSubject(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.summary == null || event.summary!.isEmpty
+        ? 'No Title'
+        : event.summary!;
+  }
+}
+
+class GoogleAPIClient extends IOClient {
+  final Map<String, String> _headers;
+
+  GoogleAPIClient(this._headers) : super();
+
+  @override
+  Future<IOStreamedResponse> send(BaseRequest request) =>
+      super.send(request..headers.addAll(_headers));
+
+  @override
+  Future<Response> head(Uri url, {Map<String, String>? headers}) =>
+      super.head(url,
+          headers: (headers != null ? (headers..addAll(_headers)) : headers));
 }
