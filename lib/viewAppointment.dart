25a7edf30bc4794df4ad584d8582f34de061095c
Bdoor Nasser <bdoorna7@gmail.com>
2023-10-02 20:41:39 +0300

    n

--- lib/viewAppointment.dart
+++ lib/viewAppointment.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/calendar/v3.dart' as GoogleAPI;
+import 'package:http/io_client.dart' show IOClient, IOStreamedResponse;
+import 'package:http/http.dart' show BaseRequest, Response;
 
 class viewAppointment extends StatefulWidget {
   //const SignUp({Key? key}) : super(key: key);
@@ -13,6 +17,26 @@ class viewAppointment extends StatefulWidget {
 
 class _viewAppointment extends State<viewAppointment> {
   @override
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    clientId: 'your-client_id.apps.googleusercontent.com',
+    scopes: <String>[GoogleAPI.CalendarApi.calendarScope],
+  );
+
+  GoogleSignInAccount? _currentUser;
+
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      setState(() {
+        _currentUser = account;
+      });
+      if (_currentUser != null) {
+        //getGoogleEventsData();
+      }
+    });
+    _googleSignIn.signInSilently();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: SfCalendar(
