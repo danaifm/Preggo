74ec746856ab62a5c2e9177dc6a3628216baf847
Bdoor Nasser <bdoorna7@gmail.com>
2023-10-03 22:37:19 +0300

    adding the grid view

--- lib/screens/ToolsPage.dart
+++ lib/screens/ToolsPage.dart
@@ -10,6 +10,84 @@ class ToolsPage extends StatefulWidget {
 class _ToolsPage extends State<ToolsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Tools page")));
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+            margin: EdgeInsets.only(left: 20, top: 40),
+            child: RichText(
+              text: const TextSpan(
+                  style: TextStyle(
+                    fontFamily: 'Urbanist',
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Tools\n',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 35,
+                            fontWeight: FontWeight.w400)),
+                    TextSpan(
+                        text: ' Use our tools to make your journey easier',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 121, 119, 119))),
+                  ]),
+            )),
+        Container(
+          // the boxes
+          margin: EdgeInsets.only(top: 85),
+          padding: EdgeInsets.all(15), // the spaces between the boxes
+          child: GridView(
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 81, 159, 224),
+                          Color.fromARGB(255, 165, 194, 217)
+                        ],
+                        begin: Alignment.bottomRight,
+                      ),
+                      borderRadius: BorderRadius.circular(20))),
+              Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          const Color.fromARGB(255, 221, 129, 123),
+                          Color.fromARGB(255, 211, 155, 151)
+                        ],
+                        begin: Alignment.bottomRight,
+                      ),
+                      borderRadius: BorderRadius.circular(20))),
+              Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.green,
+                          Color.fromARGB(255, 142, 210, 145)
+                        ],
+                        begin: Alignment.bottomRight,
+                      ),
+                      borderRadius: BorderRadius.circular(20))),
+              Container(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          const Color.fromARGB(255, 221, 208, 95),
+                          Color.fromARGB(255, 219, 215, 186)
+                        ],
+                        begin: Alignment.bottomRight,
+                      ),
+                      borderRadius: BorderRadius.circular(20)))
+            ],
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 10,
+                childAspectRatio: 0.60),
+          ),
+        )
+      ],
+    ));
   }
 }
