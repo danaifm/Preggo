e83d385f7495e654954cdc320da573a2db1d4b0a
Nalhumaidani <113690135+Nalhumaidani@users.noreply.github.com>
2023-10-06 00:14:31 +0300

    flow modification

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -97,7 +97,7 @@ class _LoginScreenState extends State<LoginScreen> {
               Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const AddReminderScreen(),
+                    builder: (context) => NavBar() ,
                   ));
             }
           });
