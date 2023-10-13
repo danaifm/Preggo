9b9ba3eaf324ae04fa3a02b132e974e069cb222f
Dana Almoaiqel <113850532+danaifm@users.noreply.github.com>
2023-10-13 20:38:38 +0300

    اغرب شي بالحياة
    my posts not showing up only split second in all posts

--- lib/postReply.dart
+++ lib/postReply.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: use_key_in_widget_constructors, camel_case_types, unused_import, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:preggo/colors.dart';
@@ -13,131 +12,126 @@ class postReply extends StatefulWidget {
   }
 }
 
-class _postReply extends State<postReply>{
-  String getTimestamp(){
+class _postReply extends State<postReply> {
+  String getTimestamp() {
     DateTime stamp = DateTime.now();
     String formattedStamp = DateFormat('yyyy/MM/dd hh:mm a').format(stamp);
     return formattedStamp;
-
   }
-  
-  String postTitle = "What stroller should I buy?";
-  String postBody = "I'm trying to buy a new stroller for my newborn but I'm not sure which one to get. Can I get any newborn safe recommendations?"; 
-  String username = "Rana"; 
 
+  String postTitle = "What stroller should I buy?";
+  String postBody =
+      "I'm trying to buy a new stroller for my newborn but I'm not sure which one to get. Can I get any newborn safe recommendations?";
+  String username = "Rana";
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      
-
       body: Stack(
         children: [
-          
           Center(
             child: Container(
               margin: EdgeInsets.all(20),
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10,),
-              height:110,
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              height: 110,
               width: 350,
               decoration: BoxDecoration(
                 color: backGroundPink.withOpacity(0.3),
                 border: Border.all(color: backGroundPink, width: 2),
                 borderRadius: BorderRadius.circular(13),
               ),
-              child: 
-              Row(
+              child: Row(
                 children: [
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(height: 10,),
+                      SizedBox(
+                        height: 10,
+                      ),
                       Icon(
-                      Icons.account_circle_outlined,
-                      color: Colors.black,
-                      size: 38,
-                    ),
-                    Text(
-                    "$username ",
-                    style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14,
-                    fontFamily: 'Urbanist',
-                    fontWeight: FontWeight.w700,
-                    height: 1.30,
-                    letterSpacing: -0.28,
-                    ),
-                  ),
-                  
-                    ],
-                  ),
-                  SizedBox(width: 30,),
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                        postTitle,
-                        style: TextStyle(
+                        Icons.account_circle_outlined,
                         color: Colors.black,
-                        fontSize: 14,
-                        fontFamily: 'Urbanist',
-                        fontWeight: FontWeight.w800,
-                        height: 1.30,
-                        letterSpacing: -0.28,
-                        ),
+                        size: 38,
                       ),
-                      SizedBox(height: 5,),
-                      Expanded(
-                        child: Text(
-                          postBody,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 3,
-                          softWrap: true,
-                          style: TextStyle(
+                      Text(
+                        "$username ",
+                        style: TextStyle(
                           color: Colors.black,
-                          fontSize: 11,
-                          fontFamily: 'Urbanist',
-                          fontWeight: FontWeight.w600,
-                          height: 1.30,
-                          letterSpacing: -0.28,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 4,),
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Text(
-                          getTimestamp(),
-                          style: TextStyle(
-                          color: Color.fromARGB(200, 121, 113, 113),
-                          fontSize: 9,
+                          fontSize: 14,
                           fontFamily: 'Urbanist',
                           fontWeight: FontWeight.w700,
                           height: 1.30,
                           letterSpacing: -0.28,
-                          ),
                         ),
                       ),
-                          
-                      ],
-                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  Expanded(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            postTitle,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                              fontFamily: 'Urbanist',
+                              fontWeight: FontWeight.w800,
+                              height: 1.30,
+                              letterSpacing: -0.28,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Expanded(
+                            child: Text(
+                              postBody,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 3,
+                              softWrap: true,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 11,
+                                fontFamily: 'Urbanist',
+                                fontWeight: FontWeight.w600,
+                                height: 1.30,
+                                letterSpacing: -0.28,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                              getTimestamp(),
+                              style: TextStyle(
+                                color: Color.fromARGB(200, 121, 113, 113),
+                                fontSize: 9,
+                                fontFamily: 'Urbanist',
+                                fontWeight: FontWeight.w700,
+                                height: 1.30,
+                                letterSpacing: -0.28,
+                              ),
+                            ),
+                          ),
+                        ]),
                   ),
                 ],
               ),
-                    
             ),
           ),
-
-          
-          
         ],
       ),
-
     );
   }
-
 }
-
--- lib/screens/CommunityPage.dart
+++ lib/screens/CommunityPage.dart
@@ -5,6 +5,7 @@ import 'package:flutter_toggle_tab/flutter_toggle_tab.dart';
 import '../colors.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
 
 class CommunityPage extends StatefulWidget {
   const CommunityPage({super.key});
@@ -15,9 +16,21 @@ class CommunityPage extends StatefulWidget {
 int _tabTextIndexSelected = 0;
 List<String> _listTextTabToggle = ['All Posts', 'My Posts'];
 late Future<QuerySnapshot<Map<String, dynamic>>> _allPosts;
+late Future<QuerySnapshot<Map<String, dynamic>>> _myPosts;
 
 Future<QuerySnapshot<Map<String, dynamic>>> getAllPosts() async {
   var result = FirebaseFirestore.instance.collection('community').get();
+  return result;
+}
+
+Future<QuerySnapshot<Map<String, dynamic>>> getMyPosts() async {
+  User? user = FirebaseAuth.instance.currentUser;
+  String uid = user!.uid;
+  print(uid);
+  var result = FirebaseFirestore.instance
+      .collection('community')
+      .where('userID', isEqualTo: uid)
+      .get();
   return result;
 }
 
@@ -25,17 +38,17 @@ class _CommunityPage extends State<CommunityPage> {
   @override
   void initState() {
     _allPosts = getAllPosts();
+    _myPosts = getMyPosts();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
           Container(
             margin: const EdgeInsets.only(left: 20, top: 45),
-
             // child: RichText(
             //   text: const TextSpan(
             //     style: TextStyle(
@@ -57,28 +70,35 @@ class _CommunityPage extends State<CommunityPage> {
             //   ),
             // ),
           ),
-          FlutterToggleTab(
-            width: 60, // width in percent
-            borderRadius: 30,
-            height: 50,
-            selectedIndex: _tabTextIndexSelected,
-            selectedBackgroundColors: [pinkColor, pinkColor],
-            selectedTextStyle: TextStyle(
-                color: Colors.white, fontSize: 18, fontWeight: FontWeight.w700),
-            unSelectedTextStyle: TextStyle(
-                color: Colors.black87,
-                fontSize: 14,
-                fontWeight: FontWeight.w500),
-            labels: _listTextTabToggle,
-            selectedLabelIndex: (index) {
-              setState(() {
-                _tabTextIndexSelected = index;
-                print(_tabTextIndexSelected);
-              });
-            },
-            isScroll: false,
-          ),
           communityWidget(_tabTextIndexSelected),
+          Container(
+            padding: EdgeInsets.only(top: 50, left: 65),
+            child: FlutterToggleTab(
+              width: 60, // width in percent
+              borderRadius: 30,
+              height: 50,
+              selectedIndex: _tabTextIndexSelected,
+              selectedBackgroundColors: [pinkColor, pinkColor],
+              selectedTextStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700),
+              unSelectedTextStyle: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500),
+              labels: _listTextTabToggle,
+              selectedLabelIndex: (index) {
+                setState(
+                  () {
+                    _tabTextIndexSelected = index;
+                    print(_tabTextIndexSelected);
+                  },
+                );
+              },
+              isScroll: false,
+            ),
+          ),
         ], //column children
       ),
     );
@@ -98,35 +118,307 @@ Widget communityWidget(_tabTextIndexSelected) {
 Widget allPosts() {
   return Container(
     child: FutureBuilder(
-        future: _allPosts,
-        builder: (context, snapshot) {
-          if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
-            //there are posts
-            List allPosts = snapshot.data!.docs; //TODO: SORT BY LATEST
-            return SingleChildScrollView(
-              physics: AlwaysScrollableScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              child: Container(
-                height: 505,
-                child: ListView.builder(
-                  itemCount: allPosts.length,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return Text('hi');
-                  },
-                ),
+      future: _allPosts,
+      builder: (context, snapshot) {
+        if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+          //there are posts
+          List allPosts = snapshot.data!.docs; //TODO: SORT BY LATEST
+          return SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            child: Container(
+              height: 800,
+              child: ListView.builder(
+                itemCount: allPosts.length + 2,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return Container(
+                      height: 70,
+                    );
+                  } else if (index > 0 && index < allPosts.length + 1) {
+                    String username = allPosts[index - 1].data()['username'];
+                    String postTitle = allPosts[index - 1].data()['title'];
+                    String postBody = allPosts[index - 1].data()['body'];
+                    String stamp = allPosts[index - 1].data()['timestamp'];
+                    return GestureDetector(
+                      onTap: () {
+                        print('tapped');
+                      }, //TODO: rana's page
+                      child: Container(
+                        margin: EdgeInsets.all(20),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 10,
+                        ),
+                        height: 110,
+                        width: 350,
+                        decoration: BoxDecoration(
+                          color: backGroundPink.withOpacity(0.3),
+                          border: Border.all(color: backGroundPink, width: 2),
+                          borderRadius: BorderRadius.circular(13),
+                        ),
+                        child: Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Icon(
+                                  Icons.account_circle_outlined,
+                                  color: Colors.black,
+                                  size: 38,
+                                ),
+                                Text(
+                                  username,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontFamily: 'Urbanist',
+                                    fontWeight: FontWeight.w700,
+                                    height: 1.30,
+                                    letterSpacing: -0.28,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: 30,
+                            ),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    postTitle,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontFamily: 'Urbanist',
+                                      fontWeight: FontWeight.w800,
+                                      height: 1.30,
+                                      letterSpacing: -0.28,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      postBody,
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 3,
+                                      softWrap: true,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 11,
+                                        fontFamily: 'Urbanist',
+                                        fontWeight: FontWeight.w600,
+                                        height: 1.30,
+                                        letterSpacing: -0.28,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 4,
+                                  ),
+                                  Align(
+                                    alignment: Alignment.bottomLeft,
+                                    child: Text(
+                                      stamp,
+                                      style: TextStyle(
+                                        color:
+                                            Color.fromARGB(200, 121, 113, 113),
+                                        fontSize: 9,
+                                        fontFamily: 'Urbanist',
+                                        fontWeight: FontWeight.w700,
+                                        height: 1.30,
+                                        letterSpacing: -0.28,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Container(height: 100);
+                  }
+                },
               ),
-            );
-          } else if (snapshot.hasData && snapshot.data!.docs.isEmpty) {
-            //there are no posts
-            return Center(child: Text('no posts'));
-          } else {
-            return Center(child: CircularProgressIndicator()); //still loading
-          }
-        }),
+            ),
+          );
+        } else if (snapshot.hasData && snapshot.data!.docs.isEmpty) {
+          //TODO: there are no posts
+          return Center(child: Text('no posts'));
+        } else {
+          return Center(
+              child: CircularProgressIndicator()); //TODO: still loading
+        }
+      },
+    ),
   );
 }
 
 Widget myPosts() {
-  return Container();
+  print('my posts');
+  return Container(
+    child: FutureBuilder(
+      future: _myPosts,
+      builder: (context, snapshot) {
+        if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+          //there are posts
+          List allPosts = snapshot.data!.docs; //TODO: SORT BY LATEST
+          return SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            child: Container(
+              height: 800,
+              child: ListView.builder(
+                itemCount: allPosts.length + 1,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return Container(
+                      height: 70,
+                    );
+                  } else if (index > 0 && index < allPosts.length) {
+                    String username = allPosts[index - 1].data()['username'];
+                    String postTitle = allPosts[index - 1].data()['title'];
+                    String postBody = allPosts[index - 1].data()['body'];
+                    String stamp = allPosts[index - 1].data()['timestamp'];
+                    return GestureDetector(
+                      onTap: () {}, //TODO: rana's page
+                      child: Container(
+                        margin: EdgeInsets.all(20),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 10,
+                        ),
+                        height: 110,
+                        width: 350,
+                        decoration: BoxDecoration(
+                          color: backGroundPink.withOpacity(0.3),
+                          border: Border.all(color: backGroundPink, width: 2),
+                          borderRadius: BorderRadius.circular(13),
+                        ),
+                        child: Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Icon(
+                                  Icons.account_circle_outlined,
+                                  color: Colors.black,
+                                  size: 38,
+                                ),
+                                Text(
+                                  username,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                    fontFamily: 'Urbanist',
+                                    fontWeight: FontWeight.w700,
+                                    height: 1.30,
+                                    letterSpacing: -0.28,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: 30,
+                            ),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    postTitle,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontFamily: 'Urbanist',
+                                      fontWeight: FontWeight.w800,
+                                      height: 1.30,
+                                      letterSpacing: -0.28,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      postBody,
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 3,
+                                      softWrap: true,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 11,
+                                        fontFamily: 'Urbanist',
+                                        fontWeight: FontWeight.w600,
+                                        height: 1.30,
+                                        letterSpacing: -0.28,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 4,
+                                  ),
+                                  Align(
+                                    alignment: Alignment.bottomLeft,
+                                    child: Text(
+                                      stamp,
+                                      style: TextStyle(
+                                        color:
+                                            Color.fromARGB(200, 121, 113, 113),
+                                        fontSize: 9,
+                                        fontFamily: 'Urbanist',
+                                        fontWeight: FontWeight.w700,
+                                        height: 1.30,
+                                        letterSpacing: -0.28,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Container(height: 100);
+                  }
+                },
+              ),
+            ),
+          );
+        } else if (snapshot.hasData && snapshot.data!.docs.isEmpty) {
+          //TODO: there are no posts
+          return Center(child: Text('no posts'));
+        } else {
+          return Center(
+              child: CircularProgressIndicator()); //TODO: still loading
+        }
+      },
+    ),
+  );
+}
+
+String getTimestamp(DateTime stamp) {
+  String formattedStamp = DateFormat('yyyy/MM/dd hh:mm a').format(stamp);
+  return formattedStamp;
 }
