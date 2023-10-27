cf47eae589e08cd0db5bb8c3f9e34addc324e8f5
RanaOsail <ranaalosail@gmail.com>
2023-10-27 14:18:42 +0300

    get replies

--- lib/postReply.dart
+++ lib/postReply.dart
@@ -34,15 +34,12 @@ class _postReply extends State<postReply>{
   }
 
 
-    ////////////////////////////////////////////
+    //ADD THE REPLY TO FIRESTORE UNDER THE POST IN SUBCOLLECTION 'REPLIES'
     Future<void> addNewReply(String post) async {
     
       final userUid = FirebaseAuth.instance.currentUser?.uid;
       FirebaseFirestore firestore = FirebaseFirestore.instance;
-      String username = '';
-      String userID = ''; 
-
-    
+      
       //ADD THE REPLY 
       if (userUid != null && _formKey.currentState!.validate()) {
 
@@ -58,19 +55,19 @@ class _postReply extends State<postReply>{
         }
 
         //GET THE REPLIER'S USERNAME
-        final DocumentReference userDocRef = firestore.collection('users').doc(userUid);
+        /*final DocumentReference userDocRef = firestore.collection('users').doc(userUid);
         final DocumentSnapshot userSnapshot = await userDocRef.get(); 
         if(userSnapshot.exists){
           final Map<String,dynamic> userData = userSnapshot.data() as Map<String,dynamic>;
           username= userData['username'];
           
-        }
+        }*/
 
         //CREATE THE SUBCOLLECTION & ADD THE REPLY 
         CollectionReference repliesRef =
         firestore.collection('community').doc(post).collection('Replies');
         repliesRef.add({
-          'username': username,
+            'userID': userUid,
             'reply': _postReplyController.text,
             'timestamp': getTimestamp(),
         })
@@ -91,6 +88,7 @@ class _postReply extends State<postReply>{
       
   }
 
+//SUCCESS DIALOG THAT SHOWS WHEN POST IS ADDED SUCCESSFULY 
   Future<dynamic> _successDialog() {
     return showDialog(
         context: context,
@@ -119,11 +117,6 @@ class _postReply extends State<postReply>{
                           decoration: const BoxDecoration(
                             shape: BoxShape.circle,
                             color: green,
-                            // color: pinkColor,
-                            // border: Border.all(
-                            //   width: 1.3,
-                            //   color: Colors.black,
-                            // ),
                           ),
                           child: const Icon(
                             Icons.check,
@@ -183,6 +176,304 @@ class _postReply extends State<postReply>{
           );
         });
   }
+
+  //DISPLAY THE POST THAT WAS CLICKED ON (PROFILE PIC, TITLE, BODY, TIMESTAMP)
+  Future<Widget> displayPost(String postid) async {
+    String username = ''; 
+    String title = '';
+    String body = '';
+    String timestamp ='';
+
+
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    final DocumentReference postDocRef = firestore.collection('community').doc(postid);
+    final DocumentSnapshot postSnapshot = await postDocRef.get(); 
+    if(postSnapshot.exists){
+      final Map<String,dynamic> postData = postSnapshot.data() as Map<String,dynamic>;
+
+      username = postData['username'];
+      title = postData['title'];
+      body = postData['body'];
+      timestamp = postData['timestamp'];
+
+      return Container(
+        
+        child: Column(
+          children: [
+            
+            Padding(
+              padding: EdgeInsets.fromLTRB(60, 40, 0, 10),
+              child: Row(
+                children: [
+                  //PROFILE PIC
+                  CircleAvatar(
+                    radius: 21,
+                    backgroundColor: backGroundPink.withOpacity(0.7),
+                    child: Text(
+                      username.substring(0,1).toUpperCase(),
+                      style: TextStyle(
+                        fontFamily: 'Urbanist',
+                        fontSize: 22,
+                        color: blackColor,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 15,),
+                  //POST TITLE 
+                  Expanded(
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                      color: pinkColor,
+                      fontSize: 21,
+                      fontFamily: 'Urbanist',
+                      fontWeight: FontWeight.w700,
+                      height: 1.30,
+                      letterSpacing: -0.28,
+                      ),
+                    ),
+                  ),
+            
+            
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 10, 15, 10),
+              child: Row(
+                
+                children: [
+                  //POST BODY
+                  Expanded(
+                    child: Text(
+                      body, 
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: blackColor,
+                        fontFamily: 'Urbanist',
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: -0.28,),
+                      )
+                  ),
+                  
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 10, 25, 0),
+              child: Row(
+                
+                children: [
+                  //TIMESTAMP
+                  Expanded(
+                    child: Text(
+                      timestamp, 
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: grayColor,
+                        fontFamily: 'Urbanist',
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: -0.28,),
+                      )
+                  ),
+                  
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    else{
+      return Container(
+        child: Text('Post Not Found', 
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 26,
+            fontFamily: 'Urbanist',
+            fontWeight: FontWeight.w600,
+            letterSpacing: -0.28,),
+          )
+      );
+    }
+
+  }
+
+  //DISPLAY ALL REPLIES OF THE POSTS 
+  Future<Widget> getReplies(String postid) async {
+    
+    QuerySnapshot result = await FirebaseFirestore.instance
+        .collection('community')
+        .doc(postid)
+        .collection('Replies')
+        .get();
+    print(result.docs.length);
+    
+    if (result.docs.isEmpty) //no replies for this date
+    {
+      return Container(
+        child: Column(
+          children: [
+            Center(
+              //notification bell image
+              child: Padding(
+                padding: EdgeInsets.only(top: 100),
+                child: Image.asset(
+                  'assets/images/comment.png',
+                  height: 70,
+                  width: 70,
+                ),
+              ),
+            ),
+            Container(
+                //message
+                margin: EdgeInsets.fromLTRB(30, 20, 30, 80),
+                child: Text(
+                  'No Replies',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontFamily: 'Urbanist',
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: -0.28,
+                  ),
+                )),
+          ],
+        ),
+      );
+    } else {
+      //there are replies for this post 
+      List reminderResult = result.docs;
+      //sort the replies based on the date and time 
+      
+
+      return SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        child: Container(
+          //margin: EdgeInsets.only(top: 150),
+          height: 400,
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: reminderResult.length,
+            itemBuilder: (context, index) {
+              String id = reminderResult[index].data()['userID'] ?? '';
+              String ReplyBody = reminderResult[index].data()['reply'] ?? '';
+              String Replytimestamp = reminderResult[index].data()['timestamp'] ?? '';
+        
+              return Container(
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                //padding: EdgeInsets.all(10),
+        
+                child: Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                
+                      ////////////////////////////
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 0,horizontal: 10),
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10,),
+                        height:100,
+                        width: 350,
+                        decoration: BoxDecoration(
+                          color: backGroundPink.withOpacity(0.3),
+                          border: Border.all(color: backGroundPink, width: 2),
+                          borderRadius: BorderRadius.circular(13),
+                        ),
+                        child: 
+                        Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(height: 10,),
+                                Icon(
+                                Icons.account_circle_outlined,
+                                color: Colors.black,
+                                size: 38,
+                              ),
+                              Text(
+                              "Rana",
+                              style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                              fontFamily: 'Urbanist',
+                              fontWeight: FontWeight.w700,
+                              height: 1.30,
+                              letterSpacing: -0.28,
+                              ),
+                            ),
+                            
+                              ],
+                            ),
+                            SizedBox(width: 30,),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  
+                                Expanded(
+                                  child: Text(
+                                    ReplyBody,
+                                    //overflow: TextOverflow.visible,
+                                    //maxLines: 3,
+                                    softWrap: true,
+                                    style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 11,
+                                    fontFamily: 'Urbanist',
+                                    fontWeight: FontWeight.w600,
+                                    height: 1.30,
+                                    letterSpacing: -0.28,
+                                    ),
+                                  ),
+                                ),
+                                //SizedBox(height: 4,),
+                                Align(
+                                  alignment: Alignment.bottomLeft,
+                                  child: Text(
+                                    Replytimestamp,
+                                    style: TextStyle(
+                                    color: Color.fromARGB(200, 121, 113, 113),
+                                    fontSize: 9,
+                                    fontFamily: 'Urbanist',
+                                    fontWeight: FontWeight.w700,
+                                    height: 1.30,
+                                    letterSpacing: -0.28,
+                                    ),
+                                  ),
+                                ),
+                                    
+                                ],
+                                ),
+                            ),
+                          ],
+                        ),
+                              
+                      ),
+                          
+                      ///////////////////////////
+                
+                      
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      );
+    }
+  }
+
+
+
+
     ///////////////////////////////////////////
 
   @override
@@ -201,7 +492,7 @@ class _postReply extends State<postReply>{
         children: [
           
           Padding(
-            padding: EdgeInsets.fromLTRB(0, 30, 0, 5),
+            padding: EdgeInsets.fromLTRB(0, 40, 0, 5),
             child: 
               IconButton(
                 onPressed: () {
@@ -213,6 +504,40 @@ class _postReply extends State<postReply>{
                 ),
             ),
           ),
+
+          Column(
+            children: [
+              FutureBuilder<Widget>(
+            future: displayPost(postId),
+            builder: (BuildContext context,
+                AsyncSnapshot<Widget> snapshot) {
+              if (snapshot.hasData) {
+                return snapshot.data!;
+              }
+                      
+              return Center(
+                //child: CircularProgressIndicator(
+                    //color: pinkColor),
+              );
+            },
+          ),
+
+          FutureBuilder<Widget>(
+            future: getReplies(postId),
+            builder: (BuildContext context,
+                AsyncSnapshot<Widget> snapshot) {
+              if (snapshot.hasData) {
+                return snapshot.data!;
+              }
+                      
+              return Container();
+            },
+          ),
+            ],
+
+          ),
+          
+
           
           Align(
             alignment: Alignment.bottomCenter,
