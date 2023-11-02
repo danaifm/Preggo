6db18e251615dd1f67fab03ccd62bec0516d3a71
Dana Almoaiqel <113850532+danaifm@users.noreply.github.com>
2023-11-02 16:52:16 +0300

    Update CommunityPage.dart

--- lib/screens/CommunityPage.dart
+++ lib/screens/CommunityPage.dart
@@ -167,9 +167,7 @@ class _CommunityPage extends State<CommunityPage> {
           //there are posts
           List allPosts = snapshot.data!.docs;
           allPosts.sort((a, b) {
-            print('data sorting by date');
-            //SORT BY LATEST
-            // Convert 'timestamp' strings to DateTime objects for comparison
+            //SORTING THE POSTS BY LATEST
             String timeA = a.data()['timestamp'] ?? '';
             String timeB = b.data()['timestamp'] ?? '';
             // Convert 'timestamp' strings to DateTime objects for comparison
@@ -195,7 +193,7 @@ class _CommunityPage extends State<CommunityPage> {
                     String username = allPosts[index - 1].data()['username'];
                     String postTitle = allPosts[index - 1].data()['title'];
                     String postBody = allPosts[index - 1].data()['body'];
-                    String stamp = allPosts[index - 1].data()['timestamp'];
+                    String timestamp = allPosts[index - 1].data()['timestamp'];
                     String comments =
                         allPosts[index - 1].data()['comments'].toString();
                     String postID = allPosts[index - 1].reference.id.toString();
@@ -311,7 +309,7 @@ class _CommunityPage extends State<CommunityPage> {
                                               child: Align(
                                                 alignment: Alignment.bottomLeft,
                                                 child: Text(
-                                                  stamp,
+                                                  timestamp,
                                                   style: TextStyle(
                                                     color: Color.fromARGB(
                                                         200, 121, 113, 113),
@@ -415,7 +413,7 @@ class _CommunityPage extends State<CommunityPage> {
       builder: (context, snapshot) {
         if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
           List myPosts = snapshot.data!.docs;
-          //there are posts
+          //Posts exist. first we sort before displaying the posts:
           myPosts.sort((a, b) {
             //SORT BY LATEST
             String timeA = a.data()['timestamp'] ?? '';
@@ -443,8 +441,7 @@ class _CommunityPage extends State<CommunityPage> {
                     String username = myPosts[index - 1].data()['username'];
                     String postTitle = myPosts[index - 1].data()['title'];
                     String postBody = myPosts[index - 1].data()['body'];
-                    String stamp = myPosts[index - 1].data()['timestamp'];
-                    print(stamp);
+                    String timestamp = myPosts[index - 1].data()['timestamp'];
                     String postID = myPosts[index - 1].reference.id.toString();
                     String comments =
                         myPosts[index - 1].data()['comments'].toString();
@@ -558,7 +555,7 @@ class _CommunityPage extends State<CommunityPage> {
                                               child: Align(
                                                 alignment: Alignment.bottomLeft,
                                                 child: Text(
-                                                  stamp,
+                                                  timestamp,
                                                   style: TextStyle(
                                                     color: Color.fromARGB(
                                                         200, 121, 113, 113),
