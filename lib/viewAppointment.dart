21e4fcf539d17ed746a0aa4d1d0203bd0463cf8b
Dana Almoaiqel <113850532+danaifm@users.noreply.github.com>
2023-11-09 21:47:23 +0300

    deleted comments

--- lib/viewAppointment.dart
+++ lib/viewAppointment.dart
@@ -546,6 +546,7 @@ class _viewAppointment extends State<viewAppointment> {
         .collection('pregnancyInfo')
         .where('ended', isEqualTo: 'false')
         .get();
+
     firestore
         .collection('users')
         .doc(userUid)
@@ -740,46 +741,6 @@ class GoogleDataSource extends CalendarDataSource {
   GoogleDataSource({required List<Event>? events}) {
     appointments = events;
   }
-
-//maybe userful for later sprints:
-  // @override
-  // DateTime getStartTime(int index) {
-  //   final Event event = appointments![index];
-  //   return event.start?.date ?? event.start!.dateTime!.toLocal();
-  // }
-
-  // @override
-  // bool isAllDay(int index) {
-  //   return appointments![index].start.date != null;
-  // }
-
-  // @override
-  // DateTime getEndTime(int index) {
-  //   final Event event = appointments![index];
-  //   return event.endTimeUnspecified != null && event.endTimeUnspecified!
-  //       ? (event.start?.date ?? event.start!.dateTime!.toLocal())
-  //       : (event.end?.date != null
-  //           ? event.end!.date!.add(const Duration(days: -1))
-  //           : event.end!.dateTime!.toLocal());
-  // }
-
-  // @override
-  // String getLocation(int index) {
-  //   return appointments![index].location ?? '';
-  // }
-
-  // @override
-  // String getNotes(int index) {
-  //   return appointments![index].description ?? '';
-  // }
-
-  // @override
-  // String getSubject(int index) {
-  //   final Event event = appointments![index];
-  //   return event.summary == null || event.summary!.isEmpty
-  //       ? 'No Title'
-  //       : event.summary!;
-  // }
 }
 
 class DataSource extends CalendarDataSource {
